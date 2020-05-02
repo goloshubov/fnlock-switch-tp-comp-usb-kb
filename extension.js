c89627c7aa1a39c2251c1b6581c53c00a8d0758a
Dmitry Goloshubov <goloshubov@gmail.com>
2020-05-02 17:38:12 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -10,7 +10,6 @@ const Mainloop = imports.mainloop;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
-
 const FNLOCKED_ICON = "fnlk.svg";
 const FNUNLOCKED_ICON = "fn.svg";
 
@@ -22,7 +21,7 @@ function init(metadata) {
 
 
 function toggleFNLK() {
-	let [ok, out, err, exit] = GLib.spawn_command_line_sync("sh -c '{ grep -q 1 /sys/bus/hid/devices/*17EF\:604*/fn_lock && echo 0 || echo 1; } | tee /sys/bus/hid/devices/*17EF\:604*/fn_lock'");
+  let [ok, out, err, exit] = GLib.spawn_command_line_sync("sh -c '{ grep -q 1 /sys/bus/hid/devices/*17EF\:604*/fn_lock && echo 0 || echo 1; } | tee /sys/bus/hid/devices/*17EF\:604*/fn_lock'");
 	
   if (ByteArray.toString(out).includes('0')) {
     icon.set_gicon(gicon_locked)
@@ -33,7 +32,7 @@ function toggleFNLK() {
 
 
 function enable() {
-	button = new PanelMenu.Button(0.0);  
+  button = new PanelMenu.Button(0.0);  
   icon = new St.Icon({ style_class: 'system-status-icon' });
   gicon_locked = Gio.icon_new_for_string(Me.path + "/icons/" + FNLOCKED_ICON);  
   gicon_unlocked = Gio.icon_new_for_string(Me.path + "/icons/" + FNUNLOCKED_ICON);
@@ -46,13 +45,12 @@ function enable() {
     icon.set_gicon(gicon_unlocked)
   }
   
-	button.actor.add_actor(icon);
-	button.actor.connect('button-press-event', toggleFNLK);
-	Main.panel.addToStatusArea('Test', button);	
+  button.actor.add_actor(icon);
+  button.actor.connect('button-press-event', toggleFNLK);
+  Main.panel.addToStatusArea('Test', button);	
 }
 
 
 function disable() {
-	button.destroy();
+  button.destroy();
 }
-
