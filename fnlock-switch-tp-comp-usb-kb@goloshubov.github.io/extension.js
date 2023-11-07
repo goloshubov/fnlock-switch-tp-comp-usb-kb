9db333f50670a6a18904034bfffe3dd89f555c59
Dmitry Goloshubov <goloshubov@gmail.com>
2023-11-08 00:22:21 +0300

    gnome 45 support

--- fnlock-switch-tp-comp-usb-kb@goloshubov.github.io/extension.js
+++ fnlock-switch-tp-comp-usb-kb@goloshubov.github.io/extension.js
@@ -1,60 +1,49 @@
-'use strict';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import St from 'gi://St';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as ExtensionUtils from 'resource:///org/gnome/shell/misc/extensionUtils.js';
 
-const St = imports.gi.St;
-const Gio = imports.gi.Gio;
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const GLib = imports.gi.GLib;
 const ByteArray = imports.byteArray;
-const Mainloop = imports.mainloop;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-
 const FNLOCKED_ICON = "fnlk.svg";
 const FNUNLOCKED_ICON = "fn.svg";
 
-let button, icon, gicon_locked, gicon_unlocked;
 
+export default class FnLockExtension extends Extension {
+    enable() {
+      const gicon_locked = Gio.icon_new_for_string(this.path + "/icons/" + FNLOCKED_ICON);
+      const gicon_unlocked = Gio.icon_new_for_string(this.path + "/icons/" + FNUNLOCKED_ICON);
 
-function init(metadata) {
-}
+      this._button = new PanelMenu.Button(0.0);
+      this._icon = new St.Icon({ style_class: 'system-status-icon' });
 
+      let [ok, out, err, exit] = GLib.spawn_command_line_sync("sh -c 'cat /sys/bus/hid/devices/*17EF\:604*/fn_lock'");
 
-function toggleFnLock() {
-  let [ok, out, err, exit] = GLib.spawn_command_line_sync("sh -c '{ grep -q 1 /sys/bus/hid/devices/*17EF\:604*/fn_lock && echo 0 || echo 1; } | tee /sys/bus/hid/devices/*17EF\:604*/fn_lock'");
+      if (ByteArray.toString(out).includes('0')) {
+        this._icon.set_gicon(gicon_unlocked)
+      } else {
+        this._icon.set_gicon(gicon_locked)
+      }
 
-  if (ByteArray.toString(out).includes('0')) {
-    icon.set_gicon(gicon_unlocked)
-  } else {
-    icon.set_gicon(gicon_locked)
-  }
-}
+      this._button.actor.add_actor(this._icon);
+      this._button.actor.connect('button-press-event', (item, event) => {
+        let [ok, out, err, exit] = GLib.spawn_command_line_sync("sh -c '{ grep -q 1 /sys/bus/hid/devices/*17EF\:604*/fn_lock && echo 0 || echo 1; } | tee /sys/bus/hid/devices/*17EF\:604*/fn_lock'");
+        if (ByteArray.toString(out).includes('0')) {
+          this._icon.set_gicon(gicon_unlocked)
+        } else {
+          this._icon.set_gicon(gicon_locked)
+        }
+      });
 
+      Main.panel.addToStatusArea('FnLock', this._button);
+    }
 
-function enable() {
-  button = new PanelMenu.Button(0.0);
-  icon = new St.Icon({ style_class: 'system-status-icon' });
-  gicon_locked = Gio.icon_new_for_string(Me.path + "/icons/" + FNLOCKED_ICON);
-  gicon_unlocked = Gio.icon_new_for_string(Me.path + "/icons/" + FNUNLOCKED_ICON);
-
-  let [ok, out, err, exit] = GLib.spawn_command_line_sync("sh -c 'cat /sys/bus/hid/devices/*17EF\:604*/fn_lock'");
-
-  if (ByteArray.toString(out).includes('0')) {
-    icon.set_gicon(gicon_unlocked)
-  } else {
-    icon.set_gicon(gicon_locked)
-  }
-
-  button.actor.add_actor(icon);
-  button.actor.connect('button-press-event', toggleFnLock);
-  Main.panel.addToStatusArea('FnLock', button);
-}
-
-
-function disable() {
-  button.destroy();
-  button = null;
-  icon = null;
-  gicon_locked = null;
-  gicon_unlocked = null;
+    disable() {
+        this._button?.destroy();
+        this._button = null;
+        this._icon?.destroy();
+        this._icon = null;
+    }
 }
