30fdab24946d11ae1a1348be7d2c1f30ca29b6e1
Dmitry Goloshubov <goloshubov@gmail.com>
2024-09-21 22:12:38 +0300

    add initial keybinding support, async subprocesses

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ Fn is unlocked\
 Fn is locked (FnLk)\
 ![screenshot01](https://github.com/goloshubov/tp-comp-keyboard-fnlk-switch/blob/master/about/screenshots/ss01.png)
 
+Keybinding:\
+<Control> + Escape
+
 ## Why?
 There is an issue with Lenovo ThinkPad Compact USB Keyboard - FnLk doesn't work by pressing Fn+Esc in Linux. Manual toggling by writing 1|0 into `/sys/bus/hid/devices/*17EF\:604*/fn_loc` is possible though, thanks to: https://github.com/lentinj/tp-compact-keyboard.
 
@@ -16,15 +19,15 @@ There is an issue with Lenovo ThinkPad Compact USB Keyboard - FnLk doesn't work 
 Notice that (GNOME) user should have write access to `/sys/bus/hid/devices/*17EF\:604*/fn_loc`
 
 ```
-cat <<'EOF' >  /etc/udev/rules.d/99-thinkpad-compact-keyboard.rules 
+cat <<'EOF' >  /etc/udev/rules.d/99-thinkpad-compact-keyboard.rules
 SUBSYSTEM=="hid", ATTRS{idVendor}=="17ef", ATTRS{idProduct}=="604*", \
     RUN += "/bin/sh -c 'chown change_to_your_username \"/sys/$devpath/fn_lock\"'"
 EOF
 ```
 or
 ```
-cat <<'EOF' >  /etc/udev/rules.d/99-thinkpad-compact-keyboard.rules 
+cat <<'EOF' >  /etc/udev/rules.d/99-thinkpad-compact-keyboard.rules
 SUBSYSTEM=="hid", ATTRS{idVendor}=="17ef", ATTRS{idProduct}=="604*", \
-    RUN += "/bin/sh -c 'chmod 0666 \"/sys/$devpath/fn_lock\"'"
+    RUN += "/bin/sh -c 'chmod 0660 \"/sys/$devpath/fn_lock\"'"
 EOF
 ```
--- fnlock-switch-tp-comp-usb-kb@goloshubov.github.io/extension.js
+++ fnlock-switch-tp-comp-usb-kb@goloshubov.github.io/extension.js
@@ -1,47 +1,111 @@
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
-import St from 'gi://St';
-import Gio from 'gi://Gio';
-import GLib from 'gi://GLib';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as ExtensionUtils from 'resource:///org/gnome/shell/misc/extensionUtils.js';
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+import St from "gi://St";
+import Gio from "gi://Gio";
+import GLib from "gi://GLib";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import Meta from "gi://Meta";
+import Shell from "gi://Shell";
+import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
+import * as ExtensionUtils from "resource:///org/gnome/shell/misc/extensionUtils.js";
 
 const FNLOCKED_ICON = "fnlk.svg";
 const FNUNLOCKED_ICON = "fn.svg";
-
+const NOFNLOCK_ICON = "none.svg";
 
 export default class FnLockExtension extends Extension {
-    enable() {
-      const gicon_locked = Gio.icon_new_for_string(this.path + "/icons/" + FNLOCKED_ICON);
-      const gicon_unlocked = Gio.icon_new_for_string(this.path + "/icons/" + FNUNLOCKED_ICON);
+  async update_fnlock(proc) {
+    try {
+      proc.communicate_utf8_async(null, null, (proc, res) => {
+        try {
+          let [ok, stdout, stderr] = proc.communicate_utf8_finish(res);
+          //console.log(stdout.toString());
 
-      this._button = new PanelMenu.Button(0.0);
-      this._icon = new St.Icon({ style_class: 'system-status-icon' });
-
-      let [ok, out, err, exit] = GLib.spawn_command_line_sync("sh -c 'cat /sys/bus/hid/devices/*17EF\:604*/fn_lock'");
-      if (out.toString().includes('0')) {
-        this._icon.set_gicon(gicon_unlocked)
-      } else {
-        this._icon.set_gicon(gicon_locked)
-      }
-
-      this._button.actor.add_child(this._icon);
-      this._button.actor.connect('button-press-event', (item, event) => {
-        let [ok, out, err, exit] = GLib.spawn_command_line_sync("sh -c '{ grep -q 1 /sys/bus/hid/devices/*17EF\:604*/fn_lock && echo 0 || echo 1; } | tee /sys/bus/hid/devices/*17EF\:604*/fn_lock'");
-        if (out.toString().includes('0')) {
-          this._icon.set_gicon(gicon_unlocked)
-        } else {
-          this._icon.set_gicon(gicon_locked)
+          if (stdout.toString().includes("1")) {
+            this._icon.set_gicon(this._gicon_locked);
+          } else if (stdout.toString().includes("0")) {
+            this._icon.set_gicon(this._gicon_unlocked);
+          } else {
+            this._icon.set_gicon(this._gicon_none);
+          }
+        } catch (e) {
+          reject(e);
         }
       });
-
-      Main.panel.addToStatusArea('FnLock', this._button);
+    } catch (e) {
+      logError(e);
     }
+  }
 
-    disable() {
-        this._button?.destroy();
-        this._button = null;
-        this._icon?.destroy();
-        this._icon = null;
+  init_fnlock() {
+    this._icon.set_gicon(this._gicon_none);
+    try {
+      const proc = Gio.Subprocess.new(
+        ["sh", "-c", "cat /sys/bus/hid/devices/*17EF:604*/fn_lock"],
+        Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+      );
+      this.update_fnlock(proc);
+    } catch (e) {
+      logError(e);
     }
+  }
+
+  switch_fnlock() {
+    try {
+      const proc = Gio.Subprocess.new(
+        [
+          "sh",
+          "-c",
+          "test -f /sys/bus/hid/devices/0003:17EF*/fn_lock && \
+            { grep -q 1 /sys/bus/hid/devices/*17EF:604*/fn_lock && echo 0 || echo 1; } | tee /sys/bus/hid/devices/*17EF:604*/fn_lock",
+        ],
+        Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+      );
+      this.update_fnlock(proc);
+    } catch (e) {
+      logError(e);
+    }
+  }
+  enable() {
+    this._button = new PanelMenu.Button(0.0);
+    this._icon = new St.Icon({ style_class: "system-status-icon" });
+    this._gicon_locked = Gio.icon_new_for_string(
+      this.path + "/icons/" + FNLOCKED_ICON,
+    );
+    this._gicon_unlocked = Gio.icon_new_for_string(
+      this.path + "/icons/" + FNUNLOCKED_ICON,
+    );
+    this._gicon_none = Gio.icon_new_for_string(
+      this.path + "/icons/" + NOFNLOCK_ICON,
+    );
+
+    this.init_fnlock();
+
+    this._button.add_child(this._icon);
+    this._button.connect("button-press-event", (item, event) => {
+      this.switch_fnlock();
+    });
+
+    Main.panel.addToStatusArea("FnLock", this._button);
+
+    this._settings = this.getSettings();
+    Main.wm.addKeybinding(
+      "keybinding",
+      this._settings,
+      Meta.KeyBindingFlags.IGNORE_AUTOREPEAT,
+      Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
+      () => this.switch_fnlock(),
+    );
+  }
+
+  disable() {
+    Main.wm.removeKeybinding("keybinding");
+    this._button?.destroy();
+    this._button = null;
+    this._icon?.destroy();
+    this._icon = null;
+    this._gicon_locked = null;
+    this._gicon_unlocked = null;
+    this._gicon_none = null;
+    this._settings = null;
+  }
 }
